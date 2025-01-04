db68d2fa82e7cf1ee3848df283beeafea85080b8
stevescilar <stevemuambi@gmail.com>
2025-01-04 11:49:27 +0300

    mobile menu navlink

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,8 @@
-
 <!-- Header -->
 <header class="bg-blue-900 text-white p-4">
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-3xl font-semibold">
-            <a href="{{url('/')}}">PataKazi</a>
+            <a href="{{ url('/') }}">PataKazi</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
             <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
@@ -13,7 +12,7 @@
             <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
             <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
             <x-button-link url="/jobs/create" icon="edit">Create Jobs</x-button-link>
-            
+
         </nav>
         <button id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl"></i>
@@ -21,15 +20,14 @@
     </div>
     <!-- Mobile Menu -->
     <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
-        <a href="{{url('/jobs')}}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a href="{{url('/jobs/saved')}}" class="block px-4 py-2 hover:bg-blue-700">Saved Jobs</a>
-        <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a href="{{url('/register')}}" class="block px-4 py-2 hover:bg-blue-700">Register</a>
-        <a href="{{url('/dashboard')}}" class="block text-white hover:underline py-2">
-            <i class="fa fa-gauge mr-1"></i> Dashboard
-        </a>
-        <a href="{{url('/jobs/create')}}" class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black">
-            <i class="fa fa-edit"></i> Create Job
-        </a>
+
+        <x-nav-link url="/" :active="request()->is('/')" :mobile="true">Home</x-nav-link>
+        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge" :mobile="true">Dashboard</x-nav-link>
+        <x-button-link url="/jobs/create" icon="edit" :block="true">Create Jobs</x-button-link>
+
     </nav>
 </header>
