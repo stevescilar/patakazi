ef8db62bbff176a773e7fa5749939eab218cad34
stevescilar <stevemuambi@gmail.com>
2025-01-12 08:21:29 +0300

    `Refactor jobs index view to use grid layout and job card component`

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,10 +1,11 @@
 <x-layout>
-    <h1 class="text-xl font-sans font-semibold">Available Jobs</h1>
-    <ul>
+
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
-            <li><a href="{{route('jobs.show', $job->id)}}"> {{ $job->title }} </a> - {{ $job->description }}</li>
+            <x-job-card :job="$job" />
         @empty
             <p>No jobs found</p>
         @endforelse
-    </ul>
+    </div>
 </x-layout>
